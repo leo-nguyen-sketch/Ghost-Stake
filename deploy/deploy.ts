943414ad285163dd2971065f425b198cb2b8b496
leo-nguyen-sketch <bvbhtg5815@outlook.com>
2025-12-24 11:31:09 +0800

    Wire up deploy script for confidential tokens

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMETH = await deploy("ConfidentialMETH", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedMZama = await deploy("ConfidentialMZama", {
+    from: deployer,
+    log: true,
+  });
+
+  const deployedStaking = await deploy("ConfidentialStaking", {
+    from: deployer,
+    log: true,
+    args: [deployedMETH.address, deployedMZama.address],
+  });
+
+  console.log(`ConfidentialMETH contract: `, deployedMETH.address);
+  console.log(`ConfidentialMZama contract: `, deployedMZama.address);
+  console.log(`ConfidentialStaking contract: `, deployedStaking.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidential_stake"; // id required to prevent reexecution
+func.tags = ["ConfidentialTokens", "ConfidentialStaking"];
